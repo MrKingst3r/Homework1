d3e85040d61bd7bebc84b1ce42b645e676f7af2e
MrKingst3r <trent.david98@gmail.com>
2023-09-16 20:12:54 -0500

    Next Step is Data Retrieval

--- HomeworkOne.py
+++ HomeworkOne.py
@@ -33,3 +33,20 @@ execute_query(conn, create_clients_table)
 # Now we insert James, John, Jack as new entries in the Clients table
 query = "INSERT INTO clients (first_name) VALUES ('James'),('John'), ('Jack')"
 execute_query(conn, query)
+
+# Inserting rows into the sales table
+query2 = """
+INSERT INTO sales(sale_id,seller,product,quantity,price) VALUES
+(1, "James", "Pen", 40, 1.99),
+(2, "James", "Notebook", 23, 2.98),
+(3, "Jack", "Pencil", 31, 0.79),
+(4, "John", "Compass", 2, 5.99),
+(5, "John", "Monitor", 1, 100.98),
+(6, "Jack", "Sharpie", 5, 2.79),
+(7, "James", "Brush", 3, 3.99),
+(8, "John", "Ruler", 1, 1.98),
+(9, "Jack", "Paper", 100, 5.79)
+"""
+execute_query(conn, query2)
+
+
